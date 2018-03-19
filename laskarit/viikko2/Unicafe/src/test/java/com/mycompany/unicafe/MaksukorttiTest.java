daf6116ac2df34c0f206fed3aec97c2db02a4c72
Lauri <lauri.pyyhkala@helsinki.fi>
2018-03-19 16:27:29 +0200

    koodailuja

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    @Test
+    public void kortinSaldoTulostuuOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(2000);
+        assertEquals("saldo: 20.10", kortti.toString());
+    }
+    @Test
+    public void rahanOttaminenToimii () {
+        kortti.lataaRahaa(2000);
+        kortti.otaRahaa(1010);
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+    @Test
+    public void saldoEiVaheneJosEiOleRiittavastaRahaa () {
+        kortti.otaRahaa(100);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    @Test
+    public void metodiPalauttaaFalseArvonJosRahatEiRiita () {
+        assertEquals(false, kortti.otaRahaa(100));
+    }
+    @Test
+    public void metodiPalauttaaTruenJosRahatRiittaa () {
+        kortti.lataaRahaa(2000);
+        assertEquals(true, kortti.otaRahaa(100));
+    }
 }
